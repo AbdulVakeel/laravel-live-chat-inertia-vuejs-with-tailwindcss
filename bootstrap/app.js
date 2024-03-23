5de2e3b0c2024bf6a4568065286e5c046a9c74b1
Abdul Vakeel <abdulvakeel@skycodelab.io>
2024-03-23 12:35:49 +0530

    update new component
    this is a add new component

---
+++ bootstrap/app.js
@@ -0,0 +1,16 @@
+// Import any necessary dependencies or modules here
+
+// Define your component
+const MyComponent = () => {
+    // Add your component logic here
+
+    // Return the component's HTML or JSX
+    return (
+        <div>
+            {/* Add your component's content here */}
+        </div>
+    );
+};
+
+// Export your component
+export default MyComponent;
